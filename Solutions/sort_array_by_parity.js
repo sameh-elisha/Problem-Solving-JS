a00d939b342bc7f233ff621cb9bf617595ba232a
SamehAwad <67038060+SamehAwad@users.noreply.github.com>
2022-03-10 14:40:53 +0200

    Create sort_array_by_parity.js

---
+++ Solutions/sort_array_by_parity.js
@@ -0,0 +1,31 @@
+/*
+Link:- 
+    https://leetcode.com/problems/sort-array-by-parity/
+    
+Example:-
+    Input: nums = [3,1,2,4]
+    Output: [2,4,3,1]
+    
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function (nums) {
+  // Filtering even numbers
+  const even = nums.filter((num) => num % 2 === 0);
+
+  // Filtering odd numbers
+  const odd = nums.filter((num) => num % 2 !== 0);
+
+  return [...even, ...odd];
+
+  // Another solution:-
+  //   const even=new Array();
+  //   const odd =new Array();
+  //   nums.forEach(num =>{
+  //       num%2===0?arr.push(num):ae.push(num)
+  //   })
+  //   return [...even,...odd];
+};
