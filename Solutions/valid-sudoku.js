2e02d11742b40339c54e8490a058c738a7c79081
SamehAwad <67038060+SamehAwad@users.noreply.github.com>
2022-03-24 13:46:55 +0200

    Update valid-sudoku.js

--- Solutions/valid-sudoku.js
+++ Solutions/valid-sudoku.js
@@ -21,45 +21,60 @@ Example:-
 
 // Return true if there are duplicates values.
 const isDuplicatesValues = (array) => {
-  rowCheck = array.filter((elm) => elm !== ".");
-  let checkUniques = new Set(rowCheck);
-  if (rowCheck.length !== checkUniques.size) return true;
+  arrayFiltering = array.filter((elm) => elm !== ".");
+  let checkUniques = new Set(arrayFiltering);
+  if (arrayFiltering.length !== checkUniques.size) return true;
   return false;
 };
 
-const isValidSudoku = (board) => {
+const rowGood = (board) => {
   let rowCheck = [];
-  let columnCheck = [];
-  let squareCheck = [[], [], []];
-  let c = 0;
-
   for (let i = 0; i < board.length; i++) {
     // Check Rows if has duplicate values
     rowCheck = isDuplicatesValues(board[i]);
     if (rowCheck) return false;
-    //--------------------------------------//
+  }
+  return true;
+};
+
+const columnGood = (board) => {
+  let columnSelect = [];
+  for (let i = 0; i < board.length; i++) {
     // Select column && every 3 element in new array
     for (let j = 0; j < 9; j++) {
-      if (j % 3 === 0 && j !== 0) c++;
-      squareCheck[c].push(board[i][j]);
-      columnCheck.push(board[j][i]);
+      columnSelect.push(board[j][i]);
     }
+    // Check Columns if has duplicate values
+    let columnCheck = isDuplicatesValues(columnSelect);
+    if (columnCheck) return false;
+    columnSelect = [];
+  }
+  return true;
+};
 
+const boxesGood = (board) => {
+  let boxesCheck = [[], [], []];
+  let c = 0;
+  for (let i = 0; i < board.length; i++) {
+    // Select every 3 element in new array
+    for (let j = 0; j < 9; j++) {
+      if (j % 3 === 0 && j !== 0) c++;
+      boxesCheck[c].push(board[i][j]);
+    }
+    // check every 3 rows is valid
     if ((i + 1) % 3 === 0) {
       for (let j = 0; j < 3; j++) {
-        let tempArray = squareCheck[j];
+        let tempArray = boxesCheck[j];
         tempArray = isDuplicatesValues(tempArray);
         if (tempArray) return false;
       }
-
-      squareCheck = [[], [], []];
+      boxesCheck = [[], [], []];
     }
-
     c = 0;
-    // Check Columns if has duplicate values
-    let val = isDuplicatesValues(columnCheck);
-    if (val) return false;
-    columnCheck = [];
   }
   return true;
+};
+
+const isValidSudoku = (board) => {
+  return rowGood(board) && columnGood(board) && boxesGood(board);
 };
